e02f4928170df939cfa6993d9d3e895bddcf6e0d
Liruns <83442060+Liruns@users.noreply.github.com>
2025-08-17 14:00:55 +0900

    feat: add context menu to log current tab url

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,25 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log('DevTool React Extension installed');
+
+  // Create the top-level context menu
+  chrome.contextMenus.create({
+    id: 'devtoolRoot',
+    title: 'DevTool Menu',
+    contexts: ['all']
+  });
+
+  // Create the sub-menu item that prints the current tab URL
+  chrome.contextMenus.create({
+    id: 'printTabUrl',
+    parentId: 'devtoolRoot',
+    title: 'Print Current Tab URL',
+    contexts: ['all']
+  });
+});
+
+// Handle context menu clicks
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'printTabUrl' && tab) {
+    console.log(tab.url);
+  }
 });
